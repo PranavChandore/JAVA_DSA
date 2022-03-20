f5f03ccc30ba8d38cb6fae59c5d4cfc3753d0c13
Pranav Chandore <92688004+PranavChandore@users.noreply.github.com>
2022-03-20 02:44:54 -0400

    Conditions switch cases

---
+++ Conditions.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Conditions {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        
+    int Button = sc.nextInt();
+
+
+    switch(Button){
+        case 1: System.out.println("Hello");
+            break;
+        
+        case 2 :
+            System.out.println("Namaste");
+            break;
+        case 3 :
+            System.out.println("Bonjour");
+            break;
+        default:
+            System.out.println("Please Put valid number");
+            
+
+    
+    }
+    }
+}
